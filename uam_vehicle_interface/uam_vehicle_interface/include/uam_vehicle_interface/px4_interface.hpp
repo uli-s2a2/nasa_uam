e944d2d7316f5681e8e13a047289f32c2657db30
Christian Llanes <christian.llanes@gatech.edu>
2023-03-14 13:12:41 -0400

    change max thrust

--- uam_vehicle_interface/uam_vehicle_interface/include/uam_vehicle_interface/px4_interface.hpp
+++ uam_vehicle_interface/uam_vehicle_interface/include/uam_vehicle_interface/px4_interface.hpp
@@ -58,7 +58,7 @@ private:
 	bool offboard_control_enable_{false};
 	uint8_t offboard_counter_{0};
 	float mass_{0.73f};
-	float max_thrust_{53.0f};
+	float max_thrust_{50.0f};
 	float min_thrust_{0.1079f};
 	float motor_constant_{0.00000584};
 	float motor_velocity_armed_{100.0f};
